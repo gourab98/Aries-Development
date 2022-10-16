522b567c4aa4bd60446d68a34d9a6a46e4f21427
gourab98 <gourabsahag@gmail.com>
2022-10-17 01:52:24 +0600

    QR code added.

--- aries/Client/my-app/src/components/startingUI.js
+++ aries/Client/my-app/src/components/startingUI.js
@@ -1,11 +1,11 @@
-import React, {useState, useEffect} from "react";
+import React, {useEffect ,useState} from "react";
 import Styles from "./startingUI.module.css";
 import QRCode from 'qrcode';
 
 const StartingUI = () => {
 
   const [data, setData] = useState("");
-  const [qrImage, setQrImage] = useState();
+  const [qrcode, setQrcode] = useState("");
 
   const handleSubmit = async (e) => {
     console.log("hello");
@@ -26,16 +26,31 @@ const StartingUI = () => {
     }
   };
 
+  const GenerateQRCode =() =>{
+    // useEffect(()=>{
+    //   QRCode.toDataURL(data).then((setQrcode));
+    // },[])
+    console.log(qrcode)
+
+    QRCode.toDataURL(data,(err,data)=>{
+      if(err) return console.error(err)
+
+      console.log(data)
+      setQrcode(data)
+    })
+  }
+
 
   return (
     <div className={Styles.main}>
       <form onSubmit={handleSubmit}>
       <div className={Styles.container}>
         <div className={Styles.childDiv}>
-          <button className={Styles.button} type="submit">Create Invitation</button>
+          <button className={Styles.button} type="submit" onClick={GenerateQRCode}>Create Invitation</button>
           <button className={Styles.button}>Release Credential</button>
           <button className={Styles.button}>Send Proof Request</button>
           {/* <img src={qrImage} alt=""/> */}
+          <img className={Styles.image} src={qrcode}/>
         </div>
       </div>
       </form>
